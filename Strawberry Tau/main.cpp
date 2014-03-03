6a435fa576d50542798c6532fb150787ba021a0b
Alan Weide <alan@Alans-MacBook-Air.local>
2014-03-03 14:07:54 -0500

    Initial Commit

---
+++ Strawberry Tau/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Strawberry Tau
+//
+//  Created by Alan Weide on 3/3/14.
+//  Copyright (c) 2014 Alan Weide. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
