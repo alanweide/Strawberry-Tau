d1fae18447832b232dd0ae9606bd567ab8314c5c
alanweide <alan.d.weide@gmail.com>
2014-04-05 19:58:27 -0400

    Skeleton for fpu.cpp direct from alu.cpp (with appropriate changes) and addition of Sqrt, Sin, and Exp function bodies

--- fpu.cpp
+++ fpu.cpp
@@ -1,9 +1,44 @@
-//
-//  fpu.cpp
-//  Strawberry Tau
-//
-//  Created by Alan Weide on 4/5/14.
-//
-//
-
 #include "fpu.h"
+
+FPU::FPU()
+{
+	
+}
+
+double FPU::Add(double a, double b)
+{
+	return a+b;
+}
+
+double FPU::Subtract(double a, double b)
+{
+	return a-b;
+}
+
+double FPU::Multiply(double a, double b)
+{
+	return a*b;
+}
+
+double* FPU::Divide(double a, double b)
+{
+	double quotient=a/b;
+	double remainder=a%b;
+	double answer[2]={quotient,remainder};
+	return answer;
+}
+
+double FPU::SquareRoot(double a)
+{
+	// implement Taylor series for square root
+}
+
+double FPU::Sin(double a)
+{
+	// implement Taylor series for Sine
+}
+
+double FPU::Exp(double a)
+{
+	// implement Taylor series for Exponent
+}
